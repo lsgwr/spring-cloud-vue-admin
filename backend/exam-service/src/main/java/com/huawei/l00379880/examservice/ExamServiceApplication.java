40cc3a9fc259a5fe163380312208bf2192851ef4
liang shan guang <1648266192@qq.com>
2018-12-23 21:16:50 +0800

    add exam-service

---
+++ backend/exam-service/src/main/java/com/huawei/l00379880/examservice/ExamServiceApplication.java
@@ -0,0 +1,14 @@
+package com.huawei.l00379880.examservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamServiceApplication.class, args);
+    }
+
+}
+
---
+++ backend/exam-service/src/test/java/com/huawei/l00379880/examservice/ExamServiceApplicationTests.java
@@ -0,0 +1,17 @@
+package com.huawei.l00379880.examservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExamServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+

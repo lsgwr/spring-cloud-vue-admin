e30d1a791a2ed2f66127495a5c89521ac87904f7
liang shan guang <1648266192@qq.com>
2018-12-28 20:44:53 +0800

    add app for homepage

---
+++ frontend/home/src/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                最简单的react案例，可用来当所有react项目的基础模板
+            </div>
+        );
+    }
+}
+
+export default App;

c690d58ed1c38d86b5953ca8a0bc7d1e35bb4427
19920625lsg <1648266192@qq.com>
2019-11-17 00:03:09 +0800

    项目初始化完成

--- code/backend/admin/src/main/java/com/huawei/l00379880/admin/AdminApplication.java
+++ code/backend/admin/src/main/java/com/huawei/l00379880/admin/AdminApplication.java
@@ -1,3 +1,9 @@
+/***********************************************************
+ * @Description : admin的入口类
+ * @author      : 梁山广(Liang Shan Guang)
+ * @date        : 2019/11/17 0:02
+ * @email       : liangshanguang2@gmail.com
+ ***********************************************************/
 package com.huawei.l00379880.admin;
 
 import org.springframework.boot.SpringApplication;
@@ -6,8 +12,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AdminApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AdminApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
 
 }

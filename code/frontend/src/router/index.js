9597836fb78f6f51afb8ecdbf217b8e0f0bbf38c
19920625lsg <1648266192@qq.com>
2019-11-19 23:43:30 +0800

    路由router和scss的使用

--- code/frontend/src/router/index.js
+++ code/frontend/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '../views/Home'
+import NotFound from '../views/404'
+import Login from '../views/Login'
 
 Vue.use(VueRouter)
 
@@ -17,6 +19,16 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
+  },
+  {
+    path: '/404',
+    name: '404',
+    component: NotFound
   }
 ]
 

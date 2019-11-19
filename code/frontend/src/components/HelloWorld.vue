be866232cc262b24880a53b13c670192d49209b4
19920625lsg <1648266192@qq.com>
2019-11-19 23:17:27 +0800

    引入Element-UI

--- code/frontend/src/components/HelloWorld.vue
+++ code/frontend/src/components/HelloWorld.vue
@@ -7,6 +7,7 @@
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
     <h3>Installed CLI Plugins</h3>
+    <el-button type="primary">主要按钮</el-button>
     <ul>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>

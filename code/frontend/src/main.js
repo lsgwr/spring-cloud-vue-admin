85dbe70b81a334b2ff4be175a1015f266ea1d915
19920625lsg <1648266192@qq.com>
2019-11-21 00:44:02 +0800

    挂载api和global模块到Vue的全局配置中

--- code/frontend/src/main.js
+++ code/frontend/src/main.js
@@ -1,13 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import api from './http'
+import global from './utils/global'
 import store from './store'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
 
-Vue.use(ElementUI)
+Vue.use(api) // 注册使用API模块
+
+Vue.use(ElementUI) // 注册使用ElementUI模块
+
+Vue.prototype.global = global // 挂载全局配置模块
 
 new Vue({
   router,

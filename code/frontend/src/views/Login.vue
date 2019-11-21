21949c22d7e26724eabccb5b7612300a8f61e644
19920625lsg <1648266192@qq.com>
2019-11-21 08:06:35 +0800

    登录

--- code/frontend/src/views/Login.vue
+++ code/frontend/src/views/Login.vue
@@ -6,11 +6,24 @@
 </template>
 
 <script>
+import Cookies from 'js-cookie'
+import router from '../router'
+
 export default {
   name: 'Login',
   methods: {
     login () {
-      this.$router.push('/')
+      const data = {}
+      data.account = 'admin'
+      data.captcha = 'an475'
+      data.password = 'admin'
+      this.$api.login.login(data).then(function (res) {
+        alert(res.data.token)
+        Cookies.set('token', res.data.token)
+        router.push('/')
+      }).catch(function (error) {
+        alert(error)
+      })
     }
   }
 }

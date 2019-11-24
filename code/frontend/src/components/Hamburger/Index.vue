640d4c5ba6e6fc6cd797af4bb484f865c5f17ad7
19920625lsg <1648266192@qq.com>
2019-11-25 00:26:36 +0800

    收缩组件

---
+++ code/frontend/src/components/Hamburger/Index.vue
@@ -0,0 +1,16 @@
+<template>
+  <!-- 导航栏收缩组件,点击和样式类绑定都是通过父组件传值的 -->
+  <svg t="1492500959545">
+
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'Hamburger'
+}
+</script>
+
+<style scoped>
+
+</style>

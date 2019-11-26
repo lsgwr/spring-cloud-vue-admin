630bc0b135ac0389678326457a6c4eb708b31d14
19920625lsg <1648266192@qq.com>
2019-11-26 22:46:25 +0800

    权限封装，实现了按钮和表格的配套实现

---
+++ code/frontend/src/components/Core/KtButton.vue
@@ -0,0 +1,60 @@
+<template>
+  <el-button :size="size" :type="type" :icon="icon" :loading="loading" :disabled="!hasPerms(perms)" @click="handleClick">
+    {{ label }}
+  </el-button>
+</template>
+
+<script>
+import { hasPermission } from '../../utils/permission'
+
+export default {
+  name: 'KtButton',
+  data () {
+    return {}
+  },
+  props: {
+    label: { // 按钮显示文本
+      type: String,
+      default: 'Button'
+    },
+    icon: { // 按钮显示图标
+      type: String,
+      default: ''
+    },
+    size: { // 按钮尺寸
+      type: String,
+      default: null
+    },
+    type: { // 按钮类型
+      type: String,
+      default: null
+    },
+    loading: { // 是否在加载中
+      type: Boolean,
+      default: false
+    },
+    disabled: { // 按钮是否禁用
+      type: Boolean,
+      default: false
+    },
+    perms: { // 按钮权限标识
+      type: String,
+      default: null
+    }
+  },
+  methods: {
+    handleClick () {
+      // 按钮事件操处理函数
+      this.$emit('click', {})
+    },
+    hasPerms (perms) {
+      // 根据外部传入的权限标识和外部指示状态进行权限判断
+      return hasPermission(perms)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ code/frontend/src/utils/permission.js
@@ -0,0 +1,20 @@
+import store from '../store'
+
+/**
+ * 判断当前用户是否有操作权限。
+ * 原理：根据传入的权限标识，查看总的权限列表perms中是否有指定的权限集合
+ * @param perms 用户传入的权限集合
+ * @returns {boolean} 用户是否有权限
+ */
+export function hasPermission (perms) {
+  let hasPermission = false
+  let permissions = store.state.user.perms
+  let len = perms.length
+  for (let i = 0; i < len; i++) {
+    if (permissions[i] === perms) {
+      hasPermission = true
+      break
+    }
+  }
+  return hasPermission
+}

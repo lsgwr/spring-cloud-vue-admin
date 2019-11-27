6cb8f7e8f42c616f15de5a11dccbdd22d5f3b53f
19920625lsg <1648266192@qq.com>
2019-11-27 23:24:31 +0800

    修正命名规范问题

--- code/backend/admin/src/main/java/com/huawei/l00379880/admin/service/impl/SysMenuServiceImpl.java
+++ code/backend/admin/src/main/java/com/huawei/l00379880/admin/service/impl/SysMenuServiceImpl.java
@@ -49,23 +49,23 @@ public class SysMenuServiceImpl implements SysMenuService {
         return sysMenuMapper.findByUserName(userName);
     }
 
-    private void findChildren(List<SysMenu> SysMenus, List<SysMenu> menus, int menuType) {
-        for (SysMenu SysMenu : SysMenus) {
+    private void findChildren(List<SysMenu> sysMenus, List<SysMenu> menus, int menuType) {
+        for (SysMenu sysMenu : sysMenus) {
             List<SysMenu> children = new ArrayList<>();
             for (SysMenu menu : menus) {
                 if (menuType == 1 && menu.getType() == 2) {
                     // 如果是获取类型不需要按钮，且菜单类型是按钮的，直接过滤掉
                     continue;
                 }
-                if (SysMenu.getId() != null && SysMenu.getId().equals(menu.getParentId())) {
-                    menu.setParentName(SysMenu.getName());
-                    menu.setLevel(SysMenu.getLevel() + 1);
+                if (sysMenu.getId() != null && sysMenu.getId().equals(menu.getParentId())) {
+                    menu.setParentName(sysMenu.getName());
+                    menu.setLevel(sysMenu.getLevel() + 1);
                     if (!exists(children, menu)) {
                         children.add(menu);
                     }
                 }
             }
-            SysMenu.setChildren(children);
+            sysMenu.setChildren(children);
             children.sort((o1, o2) -> o1.getOrderNum().compareTo(o2.getOrderNum()));
             // 递归获取菜单，支持无限层级
             findChildren(children, menus, menuType);
